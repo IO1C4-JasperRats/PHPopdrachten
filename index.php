c2a284fdf72038930a5ab4e5e84c431c5977ceb1
Jasper Rats <jasper.rats@edu-kw1c.nl>
2020-02-10 11:17:56 +0100

    add first few assignments

---
+++ index.php
@@ -0,0 +1,43 @@
+<!--
+Opdracht:       (opdracht 2.1)
+Auteur:         Jasper Rats
+aanmaakdatum:   5-2-2020
+Bestandsnaam:   (index.php)
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php print "Index"; ?></title>
+    <link href="css/index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div id="header">
+    <h1>Inlever Opdrachten PHP</h1>
+</div>
+<aside id="menu">
+    <h1>Mijn opdrachten:</h1>
+    <ul>
+        <li>
+            Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="helloworld.php">Opdracht 2.1</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="">Opdracht 3.1</a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+</aside>
+
+
+</body>
+</html>
