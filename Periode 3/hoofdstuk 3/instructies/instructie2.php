32ba93b64b819b2a9b369adc4c51fddde99b41ed
Jasper Rats <jasper.rats@edu-kw1c.nl>
2020-02-18 10:53:26 +0100

    final adjustment assignment 2.2

---
+++ Periode 3/hoofdstuk 3/instructies/instructie2.php
@@ -0,0 +1,27 @@
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php print "instructie van hoofdstuk 3";?></title>
+</head>
+<body>
+<?php
+    $job = "ict'er"; //string
+    $year = 2019; //integer
+    $percentage = 17.1; //float
+
+    $subject = ["internet","computers","software"];//array
+
+    $skills = false; //boolean
+
+    if($skills == true || $year == 2019)
+    {
+        echo "you have skill my friend";
+    }
+    else
+    {
+        echo "sod of you daf twat focking noob";
+    };
+?>
+</body>
+</html>
